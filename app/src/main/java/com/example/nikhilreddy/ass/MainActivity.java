f2e629f411dcfe2e3830ac13cf2bdbd8d51b6323
Nikhil Reddy <33191229+nikhilreddy007@users.noreply.github.com>
2018-07-01 15:03:30 +0530

    added activities

--- app/src/main/java/com/example/nikhilreddy/ass/MainActivity.java
+++ app/src/main/java/com/example/nikhilreddy/ass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nikhilreddy.ass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -16,25 +17,34 @@ public class MainActivity extends AppCompatActivity {
         final CardView tutorialcard = (CardView) findViewById(R.id.tutorialcard);
         final LinearLayout tutoriallayout = (LinearLayout) findViewById(R.id.tutoriallayout);
         final LinearLayout tutorialoptionslayout = (LinearLayout) findViewById(R.id.tutorialoptionslayout);
+        final LinearLayout beginnertutoriallayout = (LinearLayout) findViewById(R.id.beginnertutoriallayout);
+        final LinearLayout intermediatetutoriallayout = (LinearLayout) findViewById(R.id.intermediatetutoriallayout);
+        final LinearLayout advancedtutoriallayout = (LinearLayout) findViewById(R.id.advancedtutoriallayout);
         final CardView articlescard = (CardView) findViewById(R.id.articlescard);
         final LinearLayout articleslayout = (LinearLayout) findViewById(R.id.articleslayout);
-        final LinearLayout articlesoptionslayout = (LinearLayout) findViewById(R.id.articlesoptionslayout);
         final CardView faqcard = (CardView) findViewById(R.id.faqcard);
         final LinearLayout faqlayout = (LinearLayout) findViewById(R.id.faqlayout);
-        final LinearLayout faqoptionslayout = (LinearLayout) findViewById(R.id.faqoptionslayout);
         final CardView quizcard = (CardView) findViewById(R.id.quizcard);
         final LinearLayout quizlayout = (LinearLayout) findViewById(R.id.quizlayout);
         final LinearLayout quizoptionslayout = (LinearLayout) findViewById(R.id.quizoptionslayout);
+        final LinearLayout easyquizlayout = (LinearLayout) findViewById(R.id.easyquizlayout);
+        final LinearLayout mediumquizlayout = (LinearLayout) findViewById(R.id.mediumquizlayout);
+        final LinearLayout hardquizlayout = (LinearLayout) findViewById(R.id.hardquizlayout);
+
+        final Intent beginnertut_intent = new Intent(this, BeginnerTutorialActivity.class);
+        final Intent intermediatetut_intent = new Intent(this, IntermediateTutorialActivity.class);
+        final Intent advancedtut_intent = new Intent(this, AdvancedTutorialActivity.class);
+        final Intent easyquiz_intent = new Intent(this, EasyQuizActivity.class);
+        final Intent mediumquiz_intent = new Intent(this, MediumQuizActivity.class);
+        final Intent hardquiz_intent = new Intent(this, HardQuizActivity.class);
+        final Intent articles_intent = new Intent(this, ArticlesActivity.class);
+        final Intent faq_intent = new Intent(this, FaqActivity.class);
 
         tutorialcard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if(tutoriallayout.getVisibility() == View.VISIBLE) {
                     tutoriallayout.setVisibility(View.GONE);
                     tutorialoptionslayout.setVisibility(View.VISIBLE);
-                    articleslayout.setVisibility(View.VISIBLE);
-                    articlesoptionslayout.setVisibility(View.GONE);
-                    faqlayout.setVisibility(View.VISIBLE);
-                    faqoptionslayout.setVisibility(View.GONE);
                     quizlayout.setVisibility(View.VISIBLE);
                     quizoptionslayout.setVisibility(View.GONE);
                 } else {
@@ -45,36 +55,20 @@ public class MainActivity extends AppCompatActivity {
         });
         articlescard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(articleslayout.getVisibility() == View.VISIBLE) {
-                    tutoriallayout.setVisibility(View.VISIBLE);
-                    tutorialoptionslayout.setVisibility(View.GONE);
-                    articleslayout.setVisibility(View.GONE);
-                    articlesoptionslayout.setVisibility(View.VISIBLE);
-                    faqlayout.setVisibility(View.VISIBLE);
-                    faqoptionslayout.setVisibility(View.GONE);
-                    quizlayout.setVisibility(View.VISIBLE);
-                    quizoptionslayout.setVisibility(View.GONE);
-                } else {
-                    articleslayout.setVisibility(View.VISIBLE);
-                    articlesoptionslayout.setVisibility(View.GONE);
-                }
+                tutoriallayout.setVisibility(View.VISIBLE);
+                tutorialoptionslayout.setVisibility(View.GONE);
+                quizlayout.setVisibility(View.VISIBLE);
+                quizoptionslayout.setVisibility(View.GONE);
+
             }
         });
         faqcard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(faqlayout.getVisibility() == View.VISIBLE) {
-                    tutoriallayout.setVisibility(View.VISIBLE);
-                    tutorialoptionslayout.setVisibility(View.GONE);
-                    articleslayout.setVisibility(View.VISIBLE);
-                    articlesoptionslayout.setVisibility(View.GONE);
-                    faqlayout.setVisibility(View.GONE);
-                    faqoptionslayout.setVisibility(View.VISIBLE);
-                    quizlayout.setVisibility(View.VISIBLE);
-                    quizoptionslayout.setVisibility(View.GONE);
-                } else {
-                    faqlayout.setVisibility(View.VISIBLE);
-                    faqoptionslayout.setVisibility(View.GONE);
-                }
+                tutoriallayout.setVisibility(View.VISIBLE);
+                tutorialoptionslayout.setVisibility(View.GONE);
+                quizlayout.setVisibility(View.VISIBLE);
+                quizoptionslayout.setVisibility(View.GONE);
+
             }
         });
         quizcard.setOnClickListener(new View.OnClickListener() {
@@ -82,16 +76,52 @@ public class MainActivity extends AppCompatActivity {
                 if(quizlayout.getVisibility() == View.VISIBLE) {
                     tutoriallayout.setVisibility(View.VISIBLE);
                     tutorialoptionslayout.setVisibility(View.GONE);
-                    articleslayout.setVisibility(View.VISIBLE);
-                    articlesoptionslayout.setVisibility(View.GONE);
-                    faqlayout.setVisibility(View.VISIBLE);
-                    faqoptionslayout.setVisibility(View.GONE);
                     quizlayout.setVisibility(View.GONE);
                     quizoptionslayout.setVisibility(View.VISIBLE);
                 } else {
                     quizlayout.setVisibility(View.VISIBLE);
                     quizoptionslayout.setVisibility(View.GONE);
                 }
+            }
+        });
+        beginnertutoriallayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(beginnertut_intent);
+            }
+        });
+        intermediatetutoriallayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(intermediatetut_intent);
+            }
+        });
+        advancedtutoriallayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(advancedtut_intent);
+            }
+        });
+        easyquizlayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(easyquiz_intent);
+            }
+        });
+        mediumquizlayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(mediumquiz_intent);
+            }
+        });
+        hardquizlayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(hardquiz_intent);
+            }
+        });
+        articleslayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(articles_intent);
+            }
+        });
+        faqlayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(faq_intent);
             }
         });
     }

5ff9062fd3634db6c3badfab4655fa730b04c588
goutam <16071a05a6@vnrvjiet.org>
2018-07-05 22:30:54 +0530

    Added events feature and made some minor adjustments

--- app/src/main/java/com/example/nikhilreddy/ass/MainActivity.java
+++ app/src/main/java/com/example/nikhilreddy/ass/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.nikhilreddy.ass;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mPager;
+    private int currentPage = 0;
+    private final Integer[] XMEN= {R.drawable.programmers_fact,R.drawable.formats,R.drawable.icon,R.drawable.ico,R.drawable.hbo};
+    private ArrayList<Integer> XMENArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         final Intent hardquiz_intent = new Intent(this, HardQuizActivity.class);
         final Intent articles_intent = new Intent(this, ArticlesActivity.class);
         final Intent faq_intent = new Intent(this, FaqActivity.class);
+
+        init();
 
         tutorialcard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -124,5 +139,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(faq_intent);
             }
         });
+    }
+
+    private void init() {
+        for(int i=0;i<XMEN.length;i++)
+            XMENArray.add(XMEN[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyAdapter(MainActivity.this,XMENArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == XMEN.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
     }
 }
